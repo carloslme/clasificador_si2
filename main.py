4e097f4a79554fc06aed501457cdc23810a5c1af
Carlos López Mejía <carloslmescom@gmail.com>
2019-05-28 14:35:34 -0500

    Modificación de path para subir imagenes /recibirImagenes

--- main.py
+++ main.py
@@ -5,28 +5,38 @@ import os
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/recibirImagenes', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            return '''
+            <!doctype html>
+            <title>Error al subir el archivo</title>
+            <h1>No se ha seleccionado ningún archivo</h1>
+            '''
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
-            return redirect(request.url)
+            return '''
+            <!doctype html>
+            <title>Error al subir el archivo</title>
+            <h1>No se ha seleccionado ningún archivo</h1>
+            '''
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
